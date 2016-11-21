4f00f4008b1842d8b3f8dbdd1c4d6377ae0a02f3
iliangogov <iliangogov@gmail.com>
2016-11-21 21:50:12 +0200

    FirstFiftyMembers.csproj added

---
+++ Topics/02. Linear-Data-Structures/homework/LinearDataStructures/FirstFiftyMembers/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace FirstFiftyMembers
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int members = 50;
+            var resultList = new List<int>();
+            string input = Console.ReadLine();
+            if (!IsStringValidNumber(input))
+            {
+                Console.WriteLine("This program works with numbers only. Refresh yorself, get a Beer!");
+            }
+
+            int n = int.Parse(input);
+            var queue = new Queue<int>();
+
+            queue.Enqueue(n);
+            resultList.Add(n);
+
+            int index = 1;
+            while (true)
+            {
+                int current = queue.Dequeue();
+
+                if (resultList.Count == members)
+                {
+                    break;
+                }
+
+                queue.Enqueue(current + 1);
+                index++;
+                resultList.Add(current + 1);
+
+                if (resultList.Count == members)
+                {
+                    break;
+                }
+
+                queue.Enqueue((2 * current) + 1);
+                index++;
+                resultList.Add((2 * current) + 1);
+
+                if (resultList.Count == members)
+                {
+                    break;
+                }
+
+                queue.Enqueue(current + 2);
+                index++;
+                resultList.Add(current + 2);
+
+                if (resultList.Count == members)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", resultList));
+        }
+
+        private static bool IsStringValidNumber(string number)
+        {
+            int parsedNumber = 1;
+            int.TryParse(number, out parsedNumber);
+            return parsedNumber != 0 ? true : false;
+        }
+    }
+}
---
+++ Topics/02. Linear-Data-Structures/homework/LinearDataStructures/Majorant/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Majorant
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter a number:");
+            string currentNumber = Console.ReadLine();
+            var sequence = new List<int>();
+            var filteredSequence = new List<int>();
+
+            while (currentNumber != string.Empty)
+            {
+                Console.WriteLine("Enter a number:");
+                int parsedNumber = int.Parse(currentNumber);
+                sequence.Add(parsedNumber);
+                currentNumber = Console.ReadLine();
+            }
+
+            var majorantCoeficient = (sequence.Count / 2) + 1;
+            bool isMajorantFound = false;
+
+            foreach (int number in sequence)
+            {
+                var currMatchCount = 0;
+
+                for (int i = 0; i < sequence.Count; i++)
+                {
+                    if (number == sequence[i])
+                    {
+                        currMatchCount += 1;
+                    }
+                }
+
+                if (!filteredSequence.Contains(number) && currMatchCount >= majorantCoeficient)
+                {
+                    isMajorantFound = true;
+                    filteredSequence.Add(number);
+                    Console.WriteLine($"Number {number} is majorant -> {currMatchCount} times");
+                }
+
+                currMatchCount = 0;
+            }
+
+            if (!isMajorantFound)
+            {
+                Console.WriteLine("No majorant in this sequence!");
+            }
+        }
+    }
+}

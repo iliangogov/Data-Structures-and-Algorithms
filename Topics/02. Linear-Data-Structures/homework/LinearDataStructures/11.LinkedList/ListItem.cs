2f2c5ea3f2188fdd3258dde17d4ed365adb680c5
iliangogov <iliangogov@gmail.com>
2016-11-23 20:04:47 +0200

    LinearDataStructures/11.LinkedList/11.LinkedList.csproj added

---
+++ Topics/02. Linear-Data-Structures/homework/LinearDataStructures/11.LinkedList/ListItem.cs
@@ -0,0 +1,19 @@
+﻿namespace _11.LinkedList
+{
+    public class ListItem<T>
+    {
+        public ListItem(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; set; }
+
+        public ListItem<T> NextItem { get; set; }
+
+        public override string ToString()
+        {
+            return this.Value.ToString();
+        }
+    }
+}
---
+++ Topics/02. Linear-Data-Structures/homework/LinearDataStructures/11.LinkedList/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _11.LinkedList
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var linkedList = new LinkedList<string>();
+
+            var firstItem = new ListItem<string>("First");
+
+            linkedList.FirstItem = firstItem;
+
+            var secondItem = new ListItem<string>("Second");
+
+            firstItem.NextItem = secondItem;
+
+            var thirdItem = new ListItem<string>("Third");
+
+            secondItem.NextItem = thirdItem;
+
+            Console.Write(string.Join("->", linkedList));
+
+            Console.WriteLine();
+        }
+    }
+}

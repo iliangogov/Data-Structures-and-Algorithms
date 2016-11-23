2d091c356ecf8478e6c0e12e6eaea09a89c48f10
iliangogov <iliangogov@gmail.com>
2016-11-23 20:36:38 +0200

    QueueImplementation/13.QueueImplementation.csproj added

---
+++ Topics/02. Linear-Data-Structures/homework/LinearDataStructures/QueueImplementation/LinkedQueue.cs
@@ -0,0 +1,31 @@
+﻿namespace QueueImplementation
+{
+    using System.Collections.Generic;
+
+    public class LinkedQueue<T>
+    {
+        private LinkedList<T> list;
+
+        public LinkedQueue()
+        {
+            this.list = new LinkedList<T>();
+        }
+
+        public void Enqueue(T item)
+        {
+            this.list.AddFirst(item);
+        }
+
+        public T Dequeue()
+        {
+            var item = this.list.Last;
+            this.list.RemoveLast();
+            return item.Value;
+        }
+
+        public int Count()
+        {
+            return this.list.Count;
+        }
+    }
+}
---
+++ Topics/02. Linear-Data-Structures/homework/LinearDataStructures/QueueImplementation/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace QueueImplementation
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var queue = new LinkedQueue<int>();
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine("Queue count remaining {0}", queue.Count());
+        }
+    }
+}

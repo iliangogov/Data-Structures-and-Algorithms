3d44c3506f8876b3619f7ce1c82af584a29f9e07
iliangogov <iliangogov@gmail.com>
2016-11-25 00:18:21 +0200

    Pattern.csproj added

---
+++ Workshop2016/WorkshopDsa/Pattern/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Text;
+
+namespace Pattern
+{
+    public class Program
+    {
+        private static string pattern = "urd";
+
+        static void Main()
+        {
+            var input = int.Parse(Console.ReadLine());
+            PrintPattern(input - 1);
+        }
+
+        private static void PrintPattern(int iterations)
+        {
+            if (iterations == 0)
+            {
+                Console.WriteLine(pattern);
+                return;
+            }
+
+            var upgradedPattern = new StringBuilder();
+
+            for (int i = pattern.Length - 1; i >= 0; i--)
+            {
+                var letter = pattern[i];
+                upgradedPattern.Append(RotateRight(letter.ToString()));
+            }
+
+            upgradedPattern.Append("u");
+
+            upgradedPattern.Append(pattern);
+
+            upgradedPattern.Append("r");
+
+            upgradedPattern.Append(pattern);
+
+            upgradedPattern.Append("d");
+
+            for (int i = pattern.Length - 1; i >= 0; i--)
+            {
+                var letter = pattern[i];
+                upgradedPattern.Append(RotateLeft(letter.ToString()));
+            }
+
+            pattern = upgradedPattern.ToString();
+
+            PrintPattern(iterations - 1);
+        }
+
+        private static string RotateLeft(string letter)
+        {
+            switch (letter)
+            {
+                case "u":
+                    return "r";
+                case "r":
+                    return "d";
+                case "d":
+                    return "l";
+                case "l":
+                    return "u";
+                default:
+                    throw new ArgumentException("Not a valid move");
+            }
+        }
+
+        private static string RotateRight(string letter)
+        {
+            switch (letter)
+            {
+                case "u":
+                    return "l";
+                case "r":
+                    return "u";
+                case "d":
+                    return "r";
+                case "l":
+                    return "d";
+                default:
+                    throw new ArgumentException("Not a valid move");
+            }
+        }
+    }
+}
+

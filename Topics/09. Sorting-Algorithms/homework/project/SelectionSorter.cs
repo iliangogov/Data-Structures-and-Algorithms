4901f182c4be36cc852e28047768e5ef22e012e4
iliangogov <iliangogov@gmail.com>
2016-12-07 23:05:43 +0200

    SortingHomework.sln done

--- Topics/09. Sorting-Algorithms/homework/project/SelectionSorter.cs
+++ Topics/09. Sorting-Algorithms/homework/project/SelectionSorter.cs
@@ -2,15 +2,31 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     public class SelectionSorter<T> : ISorter<T> where T : IComparable<T>
     {
         public void Sort(IList<T> collection)
         {
-            throw new NotImplementedException();
+            var collectionCount = collection.Count;
+            for (int i = 0; i < collectionCount - 1; i++)
+            {
+                var minIndex = i;
+                for (int j = i + 1; j < collectionCount; j++)
+                {
+                    var compare = collection[j].CompareTo(collection[minIndex]);
+                    if (compare < 0)
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                if (i != minIndex)
+                {
+                    var temp = collection[i];
+                    collection[i] = collection[minIndex];
+                    collection[minIndex] = temp;
+                }
+            }
         }
     }
 }
